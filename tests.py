ce32996fda8bf48eb6e6fee9c17a07eb189e29c9
DyShallow <dyshal@uw.edu>
2021-09-27 13:25:22 -0700

    fix unit tests to match change to dim format

--- tests.py
+++ tests.py
@@ -11,13 +11,13 @@ class TestMatrixFunctions(unittest.TestCase):
         mats_equal = parallel.matrices_equal(mat1_T, mat1_T_np)
         self.assertTrue(mats_equal)
 
-    def test_split_and_reconstruct(self):
+    def test_split_and_reconstruct_vector(self):
         matrix_size = 10
         split_count = 2
         vec = parallel.generate_vector(matrix_size)
         vec2 = parallel.generate_vector(matrix_size)
         split_vec_dims = parallel.split_matrix_dims(parallel.get_dims(vec),split_count)
-        self.assertEqual(split_vec_dims,((5,1),(5,1)))
+        self.assertEqual(split_vec_dims,[(5,1),(5,1)])
 
         split_mats = []
         for i in range(len(split_vec_dims)):
@@ -30,6 +30,18 @@ class TestMatrixFunctions(unittest.TestCase):
 
         reconstruction = parallel.reconstruct_split_matrices(split_mat1,(10,1))
         self.assertTrue(parallel.vectors_equal(vec, reconstruction))
+
+    def test_split_and_reconstruct_matrix(self):
+        dims = (10,10)
+        split_count = 4
+        split_mat_dims = parallel.split_matrix_dims(dims,split_count)
+        self.assertEqual(split_mat_dims, [(5,5),(5,5),(5,5),(5,5)])
+
+        split_mat_dims = parallel.split_matrix_dims(dims,5)
+        self.assertEqual(split_mat_dims, [(5,5),(5,5),(5,5),(5,5),(0,0)])
+
+        split_mat_dims = parallel.split_matrix_dims(dims,9)
+        self.assertEqual(split_mat_dims, [(5,5),(5,5),(5,5),(5,5),(0,0)])
 
 
 
